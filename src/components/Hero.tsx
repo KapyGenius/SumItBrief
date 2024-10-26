eff6ccb298daa689f31a24cb5df4be452a4cbc70
Donald Kapyouo <dkapyouo@gmail.com>
2024-10-26 20:51:29 +0200

    update Hero part

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,29 @@
+import { logo } from "../assets"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="w-full flex justify-between items-center pt-4 pb-10">
+          <img src={logo} alt="logo sumitbrief kapygenius" className=" w-96 object-contain" />
+          <button 
+            type="button" 
+            onClick={() => window.open('mailto:contact@kapygenius.com')}
+            className="black_btn"
+            >
+            Contact Me
+          </button>
+        </nav>
+
+        <h1 className="head_text">
+            Save Time and summarize any Website <br />
+            <span className="orange_gradient">Quick and fast</span>
+        </h1>
+
+        <h2 className="desc">
+            SumitBrief let's you summarize any article or blog post in seconds. <br />
+            Paste the URL of the website, and let SumitBrief do the rest.
+        </h2>
+    </header>
   )
 }
 
